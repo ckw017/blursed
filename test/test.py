8c0fd35745faf9f7197f4267356f37dfa76e767a
Chris Wong <chriskw.xyz@gmail.com>
2019-12-11 04:07:21 -0800

    Added test directory

---
+++ test/test.py
@@ -0,0 +1,23 @@
+# -*- coding: blursed -*-
+
+print("Multiplications")
+print(1.5(24 + 12 + 1)(238 * 3)(512))
+print(16(12 * 12) + 512)
+
+print("\nIndexing")
+sup = "Konichiwhat's up"
+
+print(7[sup])
+print(8[sup])
+print(9[sup])
+print(10[sup])
+
+ohno = [0,1,2,3,4]
+
+print("\nFloat indexing")
+print(2.7[ohno])
+print(0.5[ohno])
+
+why = [0,5,31,12]
+
+print(1.5[why])
